5d7836593a9fb7c1167889f96a74d8033e4c5ad7
mpatyele <mariana01.pp@gmail.com>
2023-05-24 17:25:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+//Armazenando o nome da empresa
+const nomeEmpresa = "Minha Empresa"
+
+//Armazenando a lista de receitas
+const receitas = [];
+
+
+// Função para exibir o nome da empresa
+function exibirNomeEmpresa() {
+    console.log("Nome da empresa:", nomeEmpresa);
+}
+
+
+//Função para cadastrar uma receita
+function cadastrarReceita(
+    id,
+    titulo,
+    nivelDificuldade,
+    ingredientes,
+    instrucaoPreparo,
+    videoPreparo,
+    vegana
+) {
+    //Verificando se o identificador já existe na lista de receitas
+    const receitaExistente = receitas.find((receita) => receita.id === id);
+    if (receitaExistente) {
+      console.log("Já existe uma receita com esse identificador.");
+      return;
+    }
+
+    //Criando o objeto da receita
+    const novaReceita = {
+        id,
+        titulo,
+        nivelDificuldade,
+        ingredientes,
+        instrucaoPreparo,
+        videoPreparo,
+        vegana
+    };
+
+    //Adicionando a receita à lista de receitas
+    receitas.push(novaReceita);
+    
+    console.log("Receita cadastrada com sucesso!")
+}
+
+//Função para exibir as receitas
+function exibirReceitas() {
+    exibirNomeEmpresa();
+    console.log("Receitas cadastradas:");
+    receitas.forEach((receita) => {
+        console.log("------------------------------");
+        console.log("Título: ", receita.titulo);
+        console.log("Ingredientes: ", receita.ingredientes);
+        console.log("Vegana: ", receita.vegana ? true : false);
+      });
+      console.log("------------------------------");
+}
+
+
+//Função para deletar uma receita
+function deletarReceita(id) {
+    const index = receitas.findIndex((receita) => receita.id === id);
+
+    if (index === -1) {
+        console.log("Receita não encontrada.");
+        return;
+    }
+
+    receitas.splice(index, 1);
+    console.log("Receita deletada com sucesso");
+}
+
+//Cadastrando receita
+cadastrarReceita(
+    1,
+    "Bolo de Chocolate",
+    "Moderada",
+    ["2 xícaras de farinha de trigo", "1 xícara de açúcar", "1 colher de fermento em pó"],
+    "1. Misture todos os ingredientes em uma tigela.\n2. Despeje em uma forma untada.\n3. Asse em forno preaquecido a 180°C por 30 minutos.",
+    "https://www.youtube.com/watch?v=abcd1234",
+    false,
+);
+
+cadastrarReceita(
+    2,
+    "Bolo de Cenoura",
+    "Moderada",
+    ["2 xícaras de farinha de trigo", "1 xícara de açúcar", "1 colher de fermento em pó", "2 cenouras"],
+    "1. Misture todos os ingredientes em uma tigela.\n2. Despeje em uma forma untada.\n3. Asse em forno preaquecido a 180°C por 30 minutos.",
+    "https://www.youtube.com/watch?v=abcd1234",
+    false,
+);
+
+cadastrarReceita(
+    3,
+    "Mingau",
+    "Simples",
+    ["500ml de leite de soja", "3 colheres de amido de milho", "2 colheres de açúcar"],
+    "1. Misture todos os ingredientes em uma panela. \n2. Leve ao fogo baixo mexendo sem parar até engrossar. Despeje em uma vasilha e sirva",
+    "https://www.youtube.com/watch?v=abcd1234",
+    true
+);
+
+
+exibirReceitas();
+deletarReceita(2);
+exibirReceitas();
+
+
+
+
+
+/**
+
+a. Um identificador único que não pode se repetir no sistema;
+b. titulo da receita;
+c. nível dificuldade ( com a escala de simples, moderada e complexa);
+d. lista de ingredientes já com a medida (Ex: 1 colher de açúcar);
+e. instruções de preparo;
+f. link do video de preparo;
+g. se é vegana ou não;*/
